99a2bcb62c2bee95cc030330fdc349d67b040c22
Tobias Legenden <tobiaslegenden@gmail.com>
2024-03-09 11:07:53 +0100

    assignment 3

---
+++ assignment_3/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿class Car {}
+class Audi : Car {}
+
+public class Task2 {
+    public static void Main (string[] args) {
+        Audi[] audi = new Audi[1];
+        Car[] car = audi;
+        car[0] = new Car();
+    }
+}
